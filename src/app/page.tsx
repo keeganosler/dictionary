fb8c12cfb0efb36ddf26ce341b07670c86618f8d
keeganosler <oslerkeegan@gmail.com>
2024-09-05 19:35:51 -0600

    build footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <SearchBox />
         <Word />
         {definitions}
-        <Footer />
+        <Footer word={'hello'} />
       </div>
     </main>
   );
