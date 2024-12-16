db2caa2ae1d4254c0b3d51726c778c56c6614b10
keeganosler <oslerkeegan@gmail.com>
2024-12-16 12:58:46 -0700

    start to build out definition component

--- src/app/components/definition.tsx
+++ src/app/components/definition.tsx
@@ -2,6 +2,21 @@ import { type Definition } from "../models/Definition";
 
 export default function Definition(props: {definitions: Definition[] | undefined}) {
     return (
-        <h1 className="font-bold">I am definition {JSON.stringify(props.definitions)}</h1>
+        <div>
+            {props.definitions?.[0].meanings.map(meaning => (
+                <>
+                    <h1 className="font-bold italic">{meaning.partOfSpeech}</h1>
+                    <h1 className="font-light">Meaning</h1>
+                    <ul className="p-4 list-disc list-inside">
+                        {meaning.definitions.map(definition => (
+                            <>
+                                <li>{definition.definition}</li>
+                            </>
+                        ))}
+                    </ul>
+                </>
+            ))}            
+        </div>
+
     )
 }
