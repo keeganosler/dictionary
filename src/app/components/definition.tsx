b972134c48572c15e008677f8657995cda7dc734
keeganosler <oslerkeegan@gmail.com>
2024-12-16 15:44:31 -0700

    finish the definitions

--- src/app/components/definition.tsx
+++ src/app/components/definition.tsx
@@ -5,15 +5,28 @@ export default function Definition(props: {definitions: Definition[] | undefined
         <div>
             {props.definitions?.[0].meanings.map(meaning => (
                 <>
-                    <h1 className="font-bold italic">{meaning.partOfSpeech}</h1>
-                    <h1 className="font-light">Meaning</h1>
+                    <div className="flex flex-row items-center gap-6">
+                        <h1 className="text-2xl font-bold italic py-6">{meaning.partOfSpeech}</h1>
+                        <hr className="w-full"></hr>
+                    </div>
+                    <h2 className="text-lg font-light">Meaning</h2>
                     <ul className="p-4 list-disc list-inside">
-                        {meaning.definitions.map(definition => (
+                        {meaning.definitions.slice(0,4).map(definition => (
                             <>
                                 <li>{definition.definition}</li>
                             </>
                         ))}
                     </ul>
+                    <div className="flex flex-row gap-6">
+                        <h2 className="text-lg font-light">Synonyms</h2>
+                        <div className="flex flex-row gap-4">
+                        {meaning.synonyms.slice(0,4).map(synonym => (
+                            <>
+                                <h2 className="text-lg font-bold text-fuchsia-600">{synonym}</h2>
+                            </>
+                        ))}
+                        </div>
+                    </div>
                 </>
             ))}            
         </div>
